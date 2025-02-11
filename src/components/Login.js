ca67db61b373fe7dc49829260dd88b8586a5796b
Brandon Bakus <brandonbakus@gmail.com>
2025-02-11 15:46:07 -0500

    YAYY

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,12 @@
+
+
+// Login.js
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  // Access the login function passed as context
+  const login = useOutletContext();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -11,11 +17,13 @@ function Login() {
       ...formData,
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
+  // Create a function that calls the login function when the form is submitted
   function handleLogin(e) {
     e.preventDefault();
-  }
+    login();
+  };
 
   return (
     <form onSubmit={handleLogin}>
@@ -40,8 +48,9 @@ function Login() {
         />      
       </div>
       <button type="submit">Login</button>
-    </form>
+  </form>
   );
-}
+};
 
 export default Login;
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
-import { NavLink} from "react-router-dom";
-import "./NavBar.css";
 
-function NavBar() {
+
+// NavBar.js
+import { NavLink} from "react-router-dom";
+import "./NavBar.css"
+function NavBar({ logout }) {
 
   return (
     <nav>
@@ -17,8 +19,11 @@ function NavBar() {
       >
         About
       </NavLink>
+      {/* Add the logout function to handle the onClick event */}
+      <button onClick={logout}>Logout</button>
     </nav>
   );
-}
+};
 
 export default NavBar;
+
